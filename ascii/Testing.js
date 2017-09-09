5cbe62fca53129b61b46d9d8bca2a8d2572228dc
Espen Overbye <espenoverbye@gmail.com>
2017-09-09 14:56:33 +0200

    working

--- ascii/Testing.js
+++ ascii/Testing.js
@@ -1,23 +1,39 @@
+function getIndex(c){
+    var n = 'A'.charCodeAt(0);
+    var m = c.toUpperCase().charCodeAt(0);
+    if (m>90||m<65) { 
+        return 26} 
+    return m - 65;
+}
+
+function printStr(pStr,h)
+{
+    for (var y=0;y<h;y++){
+        var s = '';
+        for (var x=0;x<pStr.length;x++){
+            s +=sT[y][getIndex(pStr.charAt(x))];
+        }
+        console.log(s);
+    }
+}
 var L = 4; //parseInt(readline());
 var H = 5; //parseInt(readline());
 //var T = readline();
 var sT= new Array();
 var temp = '';
-    var ROW = [' #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ',
+    var ROWs = [' #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ',
                '# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ',
                '### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ',
                '# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ',
                '# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  '];
 for (var i = 0; i < H; i++) {
     var oA= new Array();
-    for (var n=0;n<26;n++) {
-        temp = ROW[i].substr(n*L,L);
+    var ROW=ROWs[i]
+    for (var n=0;n<27;n++) {
+        temp = ROW.substr(n*L,L);
         oA.push(temp);
     }    
     sT.push(oA);
 }
-console.log(sT[0][4]);
-console.log(sT[1][4]);
-console.log(sT[2][4]);
-console.log(sT[3][4]);
-console.log(sT[4][4]);
+
+printStr('abc&def@',H);
